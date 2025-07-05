b23b37ea9971a3d724c2c0ebd09345d9995f085b
bpgualotuna <bpgualotuna@espe.edu.ec>
2025-07-05 01:38:30 -0500

    actuali

--- Relaciones/Personas.sql
+++ Relaciones/Personas.sql
@@ -19,6 +19,8 @@ references estado_civil(codigo)
 insert into personas (cedula,nombre,apellido,estado_civil_codigo)
 values ('1725914236','Brayan','Gualotuna','U')
 
+delete from personas where cedula='1709362162'
+
 select * from estado_civil
 insert into estado_civil(codigo,descripcion)
 values('U','UNION LIBRE')
@@ -26,4 +28,9 @@ insert into estado_civil(codigo,descripcion)
 values('C','CASADO')
 insert into estado_civil(codigo,descripcion)
 values('S','SOLTERO')
+
+select * from cuentas
+select * from compras
+select * from clientes
+select * from estado_civil
 
